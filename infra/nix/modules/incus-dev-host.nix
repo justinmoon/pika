8652198fa3c2c869d2e428fb767ba3eadc2658a1
Justin Moon <mail@justinmoon.com>
2026-03-17 14:16:05 -0500

    Add Incus guest image and readiness lane

---
+++ infra/nix/modules/incus-dev-host.nix
@@ -0,0 +1,31 @@
+{ pkgs, ... }:
+
+{
+  virtualisation.incus.enable = true;
+
+  boot.kernelModules = [ "vhost_vsock" ];
+
+  networking.nftables.enable = true;
+
+  networking.firewall.interfaces.tailscale0.allowedTCPPorts = [ 8443 ];
+
+  environment.systemPackages = with pkgs; [
+    incus
+    qemu-utils
+  ];
+
+  environment.etc."incus-dev.README".text = ''
+    Incus dev lane on pika-build
+
+    Private API:
+      https://pika-build:8443
+
+    Expected manual one-time setup for the managed-agent dev lane:
+      incus project create pika-managed-agents
+      incus profile create pika-agent-dev
+      incus storage volume list default
+
+    Image import helper:
+      ./scripts/incus-dev-image.sh import --remote-host pika-build
+  '';
+}
